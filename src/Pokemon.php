5d634bb5c678d418b3d403b1457ead96c1b94f5d
Ben Ronda <student@epicodus.com>
2016-03-07 11:45:21 -0800

    add passing tests for save() and getAll() methods

--- src/Pokemon.php
+++ src/Pokemon.php
@@ -1,15 +1,15 @@
 <?php
     class Pokemon{
         private $name;
-        private $number;
+        private $dex_number;
         private $height_feet;
         private $height_inches;
         private $weight;
         private $id;
 
-        function __construct($name, $number, $height_feet, $height_inches, $weight, $id = null){
+        function __construct($name, $dex_number, $height_feet, $height_inches, $weight, $id = null){
             $this->name = $name;
-            $this->number = $number;
+            $this->dex_number = $dex_number;
             $this->height_feet = $height_feet;
             $this->height_inches = $height_inches;
             $this->weight = $weight;
@@ -20,8 +20,8 @@
             return $this->name;
         }
 
-        function getNumber(){
-            return $this->number;
+        function getDexNumber(){
+            return $this->dex_number;
         }
 
         function getHeightFeet(){
@@ -38,6 +38,33 @@
 
         function getId(){
             return $this->id;
+        }
+
+        function save(){
+            $GLOBALS['DB']->exec("INSERT INTO pokemon (name, height_feet, height_inches, weight, dex_number)
+            VALUES ('{$this->getName()}', {$this->getHeightFeet()}, {$this->getHeightInches()}, {$this->getWeight()}, '{$this->getDexNumber()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll(){
+            $returned_pokemon = $GLOBALS['DB']->query("SELECT * FROM pokemon;");
+            $all = array();
+            foreach($returned_pokemon as $pokemon){
+                $name = $pokemon['name'];
+                $dex_number = $pokemon['dex_number'];
+                $height_feet = $pokemon['height_feet'];
+                $height_inches = $pokemon['height_inches'];
+                $weight = $pokemon['weight'];
+                $id = $pokemon['id'];
+                $new_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight, $id);
+
+                array_push($all, $new_pokemon);
+            }
+            return $all;
+        }
+
+        static function deleteAll(){
+            $GLOBALS['DB']->exec("DELETE FROM pokemon;");
         }
     }
 ?>
--- tests/PokemonTest.php
+++ tests/PokemonTest.php
@@ -6,46 +6,50 @@
 
     require_once "src/Pokemon.php";
 
-    // $server = 'mysql:host=localhost:8889;dbname=pokedex_test';
-    // $username = 'root';
-    // $password = 'root';
-    // $DB = new PDO($server, $username, $password);
+    $server = 'mysql:host=localhost;dbname=pokedex_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
     class PokemonTest extends PHPUnit_Framework_TestCase {
 
+        protected function tearDown(){
+            Pokemon::deleteAll();
+        }
+
         function test_getName(){
             $name = "Bulbasaur";
-            $number = "001";
+            $dex_number = "001";
             $height_feet = 2;
             $height_inches = 04;
-            $weight = "15.2 lbs";
-            $test_pokemon = new Pokemon($name, $number, $height_feet, $height_inches, $weight);
+            $weight = 15.2;
+            $test_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight);
 
             $result = $test_pokemon->getName();
 
             $this->assertEquals($name, $result);
         }
 
-        function test_getNumber(){
+        function test_getDexNumber(){
             $name = "Bulbasaur";
-            $number = "001";
+            $dex_number = "001";
             $height_feet = 2;
             $height_inches = 04;
-            $weight = "15.2 lbs";
-            $test_pokemon = new Pokemon($name, $number, $height_feet, $height_inches, $weight);
+            $weight = 15.2;
+            $test_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight);
 
-            $result = $test_pokemon->getNumber();
+            $result = $test_pokemon->getDexNumber();
 
-            $this->assertEquals($number, $result);
+            $this->assertEquals($dex_number, $result);
         }
 
         function test_getHeightFeet(){
             $name = "Bulbasaur";
-            $number = "001";
+            $dex_number = "001";
             $height_feet = 2;
             $height_inches = 04;
-            $weight = "15.2 lbs";
-            $test_pokemon = new Pokemon($name, $number, $height_feet, $height_inches, $weight);
+            $weight = 15.2;
+            $test_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight);
 
             $result = $test_pokemon->getHeightFeet();
 
@@ -54,11 +58,11 @@
 
         function test_getHeightInches(){
             $name = "Bulbasaur";
-            $number = "001";
+            $dex_number = "001";
             $height_feet = 2;
             $height_inches = 04;
-            $weight = "15.2 lbs";
-            $test_pokemon = new Pokemon($name, $number, $height_feet, $height_inches, $weight);
+            $weight = 15.2;
+            $test_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight);
 
             $result = $test_pokemon->getHeightInches();
 
@@ -67,17 +71,52 @@
 
         function test_getWeight() {
             $name = "Bulbasaur";
-            $number = "001";
+            $dex_number = "001";
             $height_feet = 2;
             $height_inches = 04;
-            $weight = "15.2 lbs";
-            $test_pokemon = new Pokemon($name, $number, $height_feet, $height_inches, $weight);
+            $weight = 15.2;
+            $test_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight);
 
             $result = $test_pokemon->getWeight();
 
             $this->assertEquals($weight, $result);
         }
 
+        function test_save(){
+            $name = "Bulbasaur";
+            $dex_number = "001";
+            $height_feet = 2;
+            $height_inches = 04;
+            $weight = 15.2;
+            $test_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight);
+            $test_pokemon->save();
+
+            $result = Pokemon::getAll();
+
+            $this->assertEquals([$test_pokemon], $result);
+        }
+
+        function test_getAll(){
+            $name = "Bulbasaur";
+            $dex_number = "001";
+            $height_feet = 2;
+            $height_inches = 04;
+            $weight = 15.2;
+            $test_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight);
+            $test_pokemon->save();
+
+            $name2 = "Charmander";
+            $dex_number2 = "004";
+            $height_feet2 = 2;
+            $height_inches2 = 00;
+            $weight2 = 18.7;
+            $test_pokemon2 = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight);
+            $test_pokemon2->save();
+
+            $result = Pokemon::getAll();
+
+            $this->assertEquals([$test_pokemon, $test_pokemon2], $result);
+        }
 
     }
 ?>
