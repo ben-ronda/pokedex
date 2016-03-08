551a53d55bfe03d2706a375c514d82ec772ac019
Afton Downey and Bri Popson <bpopson@gmail.com>
2016-03-08 11:22:51 -0800

    pass test to getPokemonByTypes

--- src/Type.php
+++ src/Type.php
@@ -126,6 +126,70 @@
             }
             return $pokemons;
         }
+
+        function getPokemonByTypes($search_type1, $search_type2)
+        {
+            $search_type1 = $search_type1->getId();
+            $search_type2 = $search_type2->getId();
+            $type1_matches = array();
+            $type2_matches = array();
+            $returned_pokemon1 = $GLOBALS['DB']->query("SELECT pokemon.* FROM types
+                JOIN pokemon_types ON (types.id = pokemon_types.type_id)
+                JOIN pokemon ON (pokemon_types.pokemon_id = pokemon.id)
+                WHERE types.id = {$search_type1};");
+            foreach($returned_pokemon1 as $pokemon) {
+                $name = $pokemon['name'];
+                $dex_number = $pokemon['dex_number'];
+                $height_feet = $pokemon['height_feet'];
+                $height_inches = $pokemon['height_inches'];
+                $weight = $pokemon['weight'];
+                $id = $pokemon['id'];
+                $new_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight, $id);
+                array_push($type1_matches, $new_pokemon);
+            }
+
+            $returned_pokemon2 = $GLOBALS['DB']->query("SELECT pokemon.* FROM types
+                JOIN pokemon_types ON (types.id = pokemon_types.type_id)
+                JOIN pokemon ON (pokemon_types.pokemon_id = pokemon.id)
+                WHERE types.id = {$search_type2};");
+
+            foreach($returned_pokemon2 as $pokemon) {
+                $name = $pokemon['name'];
+                $dex_number = $pokemon['dex_number'];
+                $height_feet = $pokemon['height_feet'];
+                $height_inches = $pokemon['height_inches'];
+                $weight = $pokemon['weight'];
+                $id = $pokemon['id'];
+                $new_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight, $id);
+                array_push($type2_matches, $new_pokemon);
+            }
+
+            $final_matches = array();
+            foreach ($type1_matches as $pokemon) {
+              if (in_array($pokemon, $type2_matches)) {
+                array_push($final_matches, $pokemon);
+              }
+            }
+            return $final_matches;
+
+
+
+            //
+            // $returned_pokemon = array_intersect($returned_pokemon1, $returned_pokemon2);
+            //
+            // $pokemon = array();
+            // foreach($returned_pokemon as $pokemon) {
+            //     $name = $pokemon['name'];
+            //     $dex_number = $pokemon['dex_number'];
+            //     $height_feet = $pokemon['height_feet'];
+            //     $height_inches = $pokemon['height_inches'];
+            //     $weight = $pokemon['weight'];
+            //     $id = $pokemon['id'];
+            //     $new_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight, $id);
+            //     array_push($pokemon, $new_pokemon);
+            // }
+            // return $pokemon;
+        }
     }
 
 ?>
--- tests/TypeTest.php
+++ tests/TypeTest.php
@@ -227,6 +227,51 @@
               $this->assertEquals([$test_pokemon, $test_pokemon2], $result);
           }
 
+          function testGetPokemonByTypes()
+          {
+              //Arrange
+              $name = "Fire";
+              $weakness = "Water";
+              $strength = "Grass";
+              $id = null;
+              $test_type = new Type($name, $weakness, $strength, $id);
+              $test_type->save();
+
+              $name2 = "Ground";
+              $weakness2 = "Water";
+              $strength2 = "Fire";
+              $id2 = null;
+              $test_type2 = new Type($name2, $weakness2, $strength2, $id);
+              $test_type2->save();
+
+              $name = "Bulbasaur";
+              $dex_number = "001";
+              $height_feet = 2;
+              $height_inches = 04;
+              $weight = 15.2;
+              $test_pokemon = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight);
+              $test_pokemon->save();
+
+              $name2 = "Charmander";
+              $dex_number2 = "004";
+              $height_feet2 = 2;
+              $height_inches2 = 00;
+              $weight2 = 18.7;
+              $test_pokemon2 = new Pokemon($name, $dex_number, $height_feet, $height_inches, $weight);
+              $test_pokemon2->save();
+
+
+              //Act
+              $test_type->addPokemon($test_pokemon);
+              $test_type2->addPokemon($test_pokemon);
+              $test_type->addPokemon($test_pokemon2);
+              $result = $test_type->getPokemonByTypes($test_type, $test_type2);
+
+
+              //Assert
+              $this->assertEquals([$test_pokemon], $result);
+          }
+
 
 
 
